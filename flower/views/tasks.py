93f53b00c0cfd799b0cf858feeeffb09472aaa3c
m.xurramov <m.xurramov@imkon.uz>
2024-09-26 11:35:30 +0500

    feat: get_filtered_tasks

--- flower/views/tasks.py
+++ flower/views/tasks.py
@@ -159,12 +159,12 @@ class TasksDataTable(BaseHandler):
         return uuid, args
 
     def get_filtered_tasks(self, events, limit=None, offset=0, sort_by=None, search=None, taskname=None, workername=None):
-        tasks = list(events.state.tasks_by_timestamp())  # Convert generator to list
+        tasks = list(events.state.tasks())
         filtered_tasks = []
 
         search_terms = parse_search_terms(search) if search else None
 
-        for task in tasks:
+        for _, task in tasks:
             if search_terms and not self.match_task(task, search_terms):
                 continue
             if taskname and taskname.lower() not in task.name.lower():
